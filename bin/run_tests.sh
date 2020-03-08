19eb8f6d477521b3ed9af6b3580e1f084d2b0589
Maurizio Tomasi <ziotom78@gmail.com>
2020-03-08 16:32:01 +0100

    Fix bug #17

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -3,10 +3,10 @@
 readonly MAX_LINE_LENGTH=88
 
 # Verify that the code is properly formatted
-black --check --line-length=${MAX_LINE_LENGTH} -q .
+poetry run black --check --line-length=${MAX_LINE_LENGTH} -q .
 
 # Check for common errors
-flake8 --max-line-length=${MAX_LINE_LENGTH}
+poetry run flake8 --max-line-length=${MAX_LINE_LENGTH}
 
 # Run the test suite
 poetry run python3 -m pytest -vv
