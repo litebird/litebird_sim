c75918917f099f1da7e3c53e6b6cbd499d6ef0c5
Maurizio Tomasi <ziotom78@gmail.com>
2020-05-17 17:55:31 +0200

    Install poetry using pip under WSL

--- bin/appveyor_wsl_install.bash
+++ bin/appveyor_wsl_install.bash
@@ -3,25 +3,22 @@
 set -o nounset
 set -o errexit
 
-readonly POETRY_PATH="$HOME/.poetry/bin/poetry"
 readonly LITEBIRD_SIM_PATH="$HOME/litebird_sim"
 
 # Update APT database and install basic Python3 tools
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-venv python3-distutils git
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-venv python3-distutils python3-pip git
 
 python3 --version
 
 # Install Poetry
-curl -sSL -o "$HOME/get-poetry.py" https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py
-python3 "$HOME/get-poetry.py" -f -y
+sudo pip install poetry
 
 # Useful for debugging
-python3 "$POETRY_PATH" debug
+poetry debug
 
 # Install the litebird_sim repository and test it
 git clone https://github.com/litebird/litebird_sim.git "$LITEBIRD_SIM_PATH"
 cd "$LITEBIRD_SIM_PATH"
-python3 "$POETRY_PATH" install --extras=docs
-
+poetry install --extras=docs
