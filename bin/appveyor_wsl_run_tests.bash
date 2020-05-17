ee220af8e87e66db17ad01fa3f647cad29903905
Maurizio Tomasi <ziotom78@gmail.com>
2020-05-17 09:54:09 +0200

    Be more verbose when running tests under WSL

--- bin/appveyor_wsl_run_tests.bash
+++ bin/appveyor_wsl_run_tests.bash
@@ -7,13 +7,14 @@ readonly MAX_LINE_LENGTH=88
 readonly LITEBIRD_SIM_PATH="$HOME/litebird_sim"
 readonly POETRY_PATH="$HOME/.poetry/bin/poetry"
 
-cd $LITEBIRD_SIM_PATH
+echo "# Entering $LITEBIRD_SIM_PATH"
+cd "$LITEBIRD_SIM_PATH"
 
-# Verify that the code is properly formatted
+echo "# Verifying that the code is properly formatted"
 python3 "$POETRY_PATH" run black --check --line-length=${MAX_LINE_LENGTH} -q .
 
-# Check for common errors
+echo "# Checking for common errors"
 python3 "$POETRY_PATH" run flake8 --max-line-length=${MAX_LINE_LENGTH}
 
-# Run the test suite
+echo "# Running the test suite"
 python3 "$POETRY_PATH" run python3 -m pytest --doctest-modules --ignore=test/mpi -vv
