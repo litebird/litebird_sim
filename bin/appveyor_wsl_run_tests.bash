336a33bf2e8d7af263f3de2fa1740d6cd5dc27b7
Maurizio Tomasi <ziotom78@gmail.com>
2020-05-18 06:57:40 +0200

    Use a trampoline to run bin/run_tests.sh

--- bin/appveyor_wsl_run_tests.bash
+++ bin/appveyor_wsl_run_tests.bash
@@ -3,18 +3,10 @@
 set -o nounset
 set -o errexit
 
-readonly MAX_LINE_LENGTH=88
 readonly LITEBIRD_SIM_PATH="$HOME/litebird_sim"
-readonly POETRY_PATH="$HOME/.poetry/bin/poetry"
 
 echo "# Entering $LITEBIRD_SIM_PATH"
 cd "$LITEBIRD_SIM_PATH"
 
-echo "# Verifying that the code is properly formatted"
-python3 "$POETRY_PATH" run black --check --line-length=${MAX_LINE_LENGTH} .
-
-echo "# Checking for common errors"
-python3 "$POETRY_PATH" run flake8 --max-line-length=${MAX_LINE_LENGTH}
-
-echo "# Running the test suite"
-python3 "$POETRY_PATH" run python3 -m pytest --doctest-modules --ignore=test/mpi -vv
+echo "# Running tests"
+bash bin/run_tests.sh
