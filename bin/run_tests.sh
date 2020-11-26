42e36ec23ee41cda65d5a8bd2f18bf8219618f74
Davide Poletti <davide.pole@gmail.com>
2020-11-26 15:01:07 +0100

    Disable doctests

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -9,7 +9,8 @@ poetry run black --check --line-length=${MAX_LINE_LENGTH} -q .
 poetry run flake8 --max-line-length=${MAX_LINE_LENGTH}
 
 # Run the test suite
-poetry run python3 -m pytest --doctest-modules --ignore=test/mpi -vv
+#poetry run python3 -m pytest --doctest-modules --ignore=test/mpi -vv
+poetry run python3 -m pytest -vv # XXX Skipping doctests while setting up GHA
 
 # Run the doctests
 poetry run make -C docs/ doctest
