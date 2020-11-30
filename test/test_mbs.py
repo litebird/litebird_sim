59608e4ab7d4f10d376f87aea379ec7949daeae5
NicolettaK <nkrach@sissa.it>
2020-11-30 19:04:48 +0100

    fixing test Mbs

--- test/test_mbs.py
+++ test/test_mbs.py
@@ -1,9 +1,12 @@
 import litebird_sim as lbs
 import healpy as hp
 import numpy as np
+from pathlib import Path
+
 
 def test_Mbs():
-    sim = lbs.Simulation(parameter_file='config_test.toml')
+    curpath = Path(__file__).parent
+    sim = lbs.Simulation(parameter_file=curpath / "config_test.toml")
     myinst = {}
     myinst['mock'] = {
         'freq': 140.,
@@ -12,5 +15,5 @@ def test_Mbs():
         'P_sens': 6.39}
     mbs = lbs.Mbs(sim, instrument=myinst)
     maps = mbs.run_all()
-    map_ref = hp.read_map('./reference_mbs.fits', (0,1,2))
+    map_ref = hp.read_map(curpath / "reference_mbs.fits", (0,1,2))
     assert np.allclose(maps['mock'], map_ref, atol=1e-6)
