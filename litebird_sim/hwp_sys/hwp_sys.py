309ec54d52d458fb1a314f3ea1912b6eb816e28b
Luca Pagano <lu.pagano@gmail.com>
2021-03-17 19:54:55 +0100

    internal rebinning mapmaking added

--- litebird_sim/hwp_sys/hwp_sys.py
+++ litebird_sim/hwp_sys/hwp_sys.py
@@ -7,6 +7,7 @@ import healpy as hp
 from astropy import constants as const
 from astropy.cosmology import Planck18_arXiv_v2 as cosmo
 
+COND_THRESHOLD = 1e10
 
 def _dBodTrj(nu):
     return 2*const.k_B.value*nu*nu*1e18/const.c.value/const.c.value
@@ -29,6 +30,7 @@ class HwpSys:
         #booo=self.parameters['booo']
         
         self.nside = 256
+        self.npix = hp.nside2npix(self.nside)
 
         self.h1 = 0.0
         self.h2 = 0.0
@@ -37,8 +39,9 @@ class HwpSys:
         self.z2 = 0.0
 
         self.integrate_in_band = False
+        self.built_map_on_the_fly = True
 
-        if (self.integrate_in_band):
+        if self.integrate_in_band:
             self.band_filename = 'inputs/MFT_band166_noxpol.txt'
             self.freqs,self.h1,self.h2,self.beta,self.z1,self.z2 = np.loadtxt(
             	self.band_filename,unpack=True,skiprows=1)
@@ -69,7 +72,7 @@ class HwpSys:
             )
 
             maps = mbs.run_all()[0]
-            self.maps = np.empty((self.nfreqs,3,12*self.nside*self.nside))
+            self.maps = np.empty((self.nfreqs,3,self.npix))
             for ifreq in range(self.nfreqs):
                 self.maps[ifreq] = maps['ch'+str(ifreq)]
             del(maps)
@@ -93,13 +96,14 @@ class HwpSys:
 
             self.maps = mbs.run_all()[0][channel.channel]
 
-        self.built_map_on_the_fly = True
-
-
     def fill_tod(self,obs,pointings,hwp_radpsec):
                 
         times = obs.get_times()
         
+        if self.built_map_on_the_fly:
+        	self.atd = np.zeros((self.npix,3))
+        	self.ata = np.zeros((self.npix,3,3))
+
         for idet in range(obs.n_detectors):
             pix = hp.ang2pix(self.nside,pointings[idet,:,0],pointings[idet,:,1])
 
@@ -107,26 +111,64 @@ class HwpSys:
             ca = np.cos(pointings[idet,:,2]+2*times*hwp_radpsec)
             sa = np.sin(pointings[idet,:,2]+2*times*hwp_radpsec)
 
-            if (self.integrate_in_band):
+            if self.integrate_in_band:
                 J11 = ((1+self.h1[:,np.newaxis])*ca**2-
                 	(1+self.h2[:,np.newaxis])*sa**2*np.exp(1j*self.beta[:,np.newaxis])-
                 	(self.z1[:,np.newaxis]+self.z2[:,np.newaxis])*ca*sa)
                 J12 = (((1+self.h1[:,np.newaxis])+(1+self.h2[:,np.newaxis])*
                 	np.exp(1j*self.beta[:,np.newaxis]))*ca*sa+
                 	self.z1[:,np.newaxis]*ca**2-self.z2[:,np.newaxis]*sa**2)
-
-                obs.tod[idet,:] = ((0.5*(np.abs(J11)**2+np.abs(J12)**2)*self.maps[:,0,pix] + 
-                    0.5*(np.abs(J11)**2-np.abs(J12)**2)*self.maps[:,1,pix] +
-                	(J11*J12.conjugate()).real*self.maps[:,2,pix]
-                	)*self.cmb2bb[:,np.newaxis]).sum(axis=0)/self.norm
+                
+                if self.built_map_on_the_fly:
+                	tod = ((0.5*(np.abs(J11)**2+np.abs(J12)**2)*self.maps[:,0,pix] + 
+                        0.5*(np.abs(J11)**2-np.abs(J12)**2)*self.maps[:,1,pix] +
+                	    (J11*J12.conjugate()).real*self.maps[:,2,pix]
+                	    )*self.cmb2bb[:,np.newaxis]).sum(axis=0)/self.norm
+                else: 
+                    obs.tod[idet,:] = ((0.5*(np.abs(J11)**2+np.abs(J12)**2)*self.maps[:,0,pix] + 
+                        0.5*(np.abs(J11)**2-np.abs(J12)**2)*self.maps[:,1,pix] +
+                	    (J11*J12.conjugate()).real*self.maps[:,2,pix]
+                	    )*self.cmb2bb[:,np.newaxis]).sum(axis=0)/self.norm
                 
             else:
                 J11 = (1+self.h1)*ca**2-(1+self.h2)*sa**2*np.exp(1j*self.beta)-(self.z1+self.z2)*ca*sa
                 J12 = ((1+self.h1)+(1+self.h2)*np.exp(1j*self.beta))*ca*sa+self.z1*ca**2-self.z2*sa**2
-                obs.tod[idet,:] = (0.5*(np.abs(J11)**2+np.abs(J12)**2)*self.maps[0,pix] + 
+
+                if self.built_map_on_the_fly:
+                	tod = (0.5*(np.abs(J11)**2+np.abs(J12)**2)*self.maps[0,pix] + 
                     0.5*(np.abs(J11)**2-np.abs(J12)**2)*self.maps[1,pix] +
                 	(J11*J12.conjugate()).real*self.maps[2,pix])
-        return   
+                else:
+                    obs.tod[idet,:] = (0.5*(np.abs(J11)**2+np.abs(J12)**2)*self.maps[0,pix] + 
+                        0.5*(np.abs(J11)**2-np.abs(J12)**2)*self.maps[1,pix] +
+                        (J11*J12.conjugate()).real*self.maps[2,pix])
+
+            if self.built_map_on_the_fly:
+                	#this needs to be changed when the profiles for the solver will be introduced
+                    self.atd[pix,0] += tod*0.5
+                    self.atd[pix,1] += tod*ca
+                    self.atd[pix,2] += tod*sa
+
+                    self.ata[pix,0,0] += 0.25
+                    self.ata[pix,1,0] += 0.5*ca
+                    self.ata[pix,2,0] += 0.5*sa
+                    self.ata[pix,1,1] += ca*ca
+                    self.ata[pix,2,1] += ca*sa
+                    self.ata[pix,2,2] += sa*sa
+
+        return
+
+    def make_map(self):
+        #mpi?
+        self.ata[:,0,1] = self.ata[:,1,0]
+        self.ata[:,0,2] = self.ata[:,2,0]
+        self.ata[:,1,2] = self.ata[:,2,1]
+        
+        cond = np.linalg.cond(self.ata)
+        res = np.full_like(self.atd, hp.UNSEEN)
+        mask = cond < COND_THRESHOLD
+        res[mask] = np.linalg.solve(self.ata[mask], self.atd[mask])
+        return res.T
 
 #    def fill_ata_atd(self,obs,pointings,hwp_radpsec):
 
