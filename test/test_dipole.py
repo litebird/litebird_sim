bf08f39dc9752b0c8e7e5bc9fac702b96d3a091b
Maurizio Tomasi <ziotom78@gmail.com>
2021-09-09 18:49:31 +0200

    Make test_dipole.py run faster
    With the new Numba-based binner, the test runs in ~1/3 of the time.

--- test/test_dipole.py
+++ test/test_dipole.py
@@ -3,26 +3,45 @@
 import litebird_sim as lbs
 import numpy as np
 import healpy as hp
-import astropy
+from numba import njit
+from astropy.time import Time
 import unittest
+
+@njit
+def bin_map(tod, pixel_indexes, binned_map, accum_map, hit_map):
+    # This is a helper function that implements a quick-and-dirty mapmaker.
+    # We implement here a simple binner that works only in temperature (unlike
+    # lbs.make_bin_maps, which solves for the I/Q/U Stokes components and it
+    # an overkill here).
+
+    for idx in range(len(accum_map)):
+        hit_map[idx] = 0
+        accum_map[idx] = 0.0
+
+    for sample_idx, pix_idx in enumerate(pixel_indexes):
+        accum_map[pix_idx] += tod[0, sample_idx]
+        hit_map[pix_idx] += 1
+
+    for idx in range(len(binned_map)):
+        binned_map[idx] = accum_map[idx] / hit_map[idx]
 
 
 def test_solar_dipole_fit():
     test = unittest.TestCase()
 
     # The purpose of this test is to simulate the motion of the spacecraft
-    # for one year (see `time_span`) and produce *two* timelines: the first
+    # for one year (see `time_span_s`) and produce *two* timelines: the first
     # is associated with variables `*_s_o` and refers to the case of a nonzero
     # velocity of the Solar System, and the second is associated with variables
     # `*_o` and assumes that the reference frame of the Solar System is the
     # same as the CMB's (so that there is no dipole).
 
-    start_time = astropy.time.Time("2022-01-01")
-    time_span = 365 * 24 * 3600
+    start_time = Time("2022-01-01")
+    time_span_s = 365 * 24 * 3600
     nside = 256
     sampling_hz = 1
 
-    sim = lbs.Simulation(start_time=start_time, duration_s=time_span)
+    sim = lbs.Simulation(start_time=start_time, duration_s=time_span_s)
 
     scanning = lbs.SpinningScanningStrategy(
         spin_sun_angle_rad=0.7853981633974483,
@@ -32,7 +51,7 @@ def test_solar_dipole_fit():
     )
 
     spin2ecliptic_quats = scanning.generate_spin2ecl_quaternions(
-        start_time, time_span, 100
+        start_time, time_span_s, 100
     )
 
     instr = lbs.InstrumentInfo(
@@ -78,28 +97,32 @@ def test_solar_dipole_fit():
     )
 
     npix = hp.nside2npix(nside)
-
-    # Let's make maps
+    pix_indexes = hp.ang2pix(nside, pointings[0, :, 0], pointings[0, :, 1])
 
     h = np.zeros(npix)
     m = np.zeros(npix)
-    pix_indexes = hp.ang2pix(nside, pointings[0, :, 0], pointings[0, :, 1])
-    pixel_occurrences = np.bincount(pix_indexes)
-    h[0 : len(pixel_occurrences)] += pixel_occurrences
-    for sample_idx, pix_idx in enumerate(pix_indexes):
-        m[pix_idx] += obs_s_o.tod[0, sample_idx]
+    map_s_o = np.zeros(npix)
+    map_o = np.zeros(npix)
 
-    map_s_o = m / h
+    bin_map(
+        tod=obs_s_o.tod,
+        pixel_indexes=pix_indexes,
+        binned_map=map_s_o,
+        accum_map=m,
+        hit_map=h,
+    )
+    import healpy
+    healpy.write_map("map_s_o.fits.gz", map_s_o, overwrite=True)
 
-    h = np.zeros(npix)
-    m = np.zeros(npix)
-    pix_indexes = hp.ang2pix(nside, pointings[0, :, 0], pointings[0, :, 1])
-    pixel_occurrences = np.bincount(pix_indexes)
-    h[0 : len(pixel_occurrences)] += pixel_occurrences
-    for sample_idx, pix_idx in enumerate(pix_indexes):
-        m[pix_idx] += obs_o.tod[0, sample_idx]
-
-    map_o = m / h
+    bin_map(
+        tod=obs_o.tod,
+        pixel_indexes=pix_indexes,
+        binned_map=map_o,
+        accum_map=m,
+        hit_map=h,
+    )
+    import healpy
+    healpy.write_map("map_o.fits.gz", map_s_o, overwrite=True)
 
     dip_map = map_s_o - map_o
 
