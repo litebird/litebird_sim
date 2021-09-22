8ac7532cabb67bb5d5c023ec57e0ea83fb0a7faf
Maurizio Tomasi <ziotom78@gmail.com>
2021-09-22 16:11:59 +0200

    Install FFTW on Mac OS X as well

---
+++ bin/install-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This script is based on the one in mpi4py's distribution
+# It is currently used by GitHub Actions
+
+set -e
+case `uname` in
+    Linux)
+        apt-get install libfftw3-dev
+        ;;
+    Darwin)
+        brew install fftw
+        ;;
+esac
