c4881800fe027d6a375e3e9cfa4a6ddf423758d5
sgiardie <grdsrn1@unife.it>
2022-03-21 16:42:34 +0100

    tests on out_of_band_analysis and hwp_sys decoupled

--- test/test_hwp_sys.py
+++ test/test_hwp_sys.py
@@ -4,7 +4,7 @@ import healpy as hp
 from astropy.time import Time
 import os
 
-def test_out_of_band():
+def test_hwp_sys():
 
     start_time = 0
     time_span_s = 10 
@@ -47,13 +47,14 @@ def test_out_of_band():
         quat=[0.0, 0.0, 1.0 / np.sqrt(2.0), 1.0 / np.sqrt(2.0)],
     )
 
-    obs_o, = sim.create_observations(detectors=[detT,detB])
+    obs_h, = sim.create_observations(detectors=[detT,detB])
 
-    pointings = lbs.scanning.get_pointings(obs_o,
+    pointings = lbs.scanning.get_pointings(obs_h,
                         spin2ecliptic_quats=spin2ecliptic_quats,
                         detector_quats=[detT.quat,detB.quat],
                         bore2spin_quat=instr.bore2spin_quat,
                                       )
+
     filepath = os.path.dirname(__file__).strip('test')+'litebird_sim/out_of_band_analysis/examples/MFT_100_h_beta_z.txt'
     mft = np.loadtxt(filepath)
 
@@ -69,23 +70,12 @@ def test_out_of_band():
     par = { 'hwp_sys':
        {'band_filename': filepath,
         'band_filename_solver': filepath,  #same as tod parameters
-        'bandpass':{
-            'band_type': 'top_hat',
-            'band_low_edge': nu[0],
-            'band_high_edge': nu[-1],
-            },
-        'bandpass_solver':{
-            'band_type': 'top_hat',
-            'band_low_edge': nu[0],
-            'band_high_edge': nu[-1],
-            },
-        'include_beam_throughput': False,
        }
       }
 
 
     sim.parameter_file = par   #setting the parameter file
-    hwp_sys_band = lbs.HwpSysAndBandpass(sim)
+    hwp_sys = lbs.HwpSys(sim)
 
     Mbsparams = lbs.MbsParameters(
                 make_cmb=True,
@@ -97,28 +87,30 @@ def test_out_of_band():
                 nside = nside
             )
 
-    hwp_sys_band.set_parameters(
+
+    hwp_sys = lbs.HwpSys(sim)
+
+    hwp_sys.set_parameters(
                        integrate_in_band=True,
                        integrate_in_band_solver=True,
                        correct_in_solver=True,
                        built_map_on_the_fly=False,
                        nside=nside,
                        Mbsparams = Mbsparams,
-                      # Channel = channelinfo
                       )
 
-    hwp_sys_band.fill_tod(obs_o,pointings,hwp_radpsec)
+    np.testing.assert_equal(hwp_sys.h1, h1)
+    np.testing.assert_equal(hwp_sys.h1s, h1)
+    np.testing.assert_equal(hwp_sys.h2, h2)
+    np.testing.assert_equal(hwp_sys.h2s, h2)
+    np.testing.assert_equal(np.cos(hwp_sys.beta), np.cos(np.deg2rad(beta)))
+    np.testing.assert_equal(np.cos(hwp_sys.betas), np.cos(np.deg2rad(beta)))
+    np.testing.assert_equal(hwp_sys.z1, z1)
+    np.testing.assert_equal(hwp_sys.z1s, z1)
+    np.testing.assert_equal(hwp_sys.z2, z2)
+    np.testing.assert_equal(hwp_sys.z2s, z2)
 
-    np.testing.assert_equal(hwp_sys_band.h1, h1)
-    np.testing.assert_equal(hwp_sys_band.h1s, h1)
-    np.testing.assert_equal(hwp_sys_band.h2, h2)
-    np.testing.assert_equal(hwp_sys_band.h2s, h2)
-    np.testing.assert_equal(hwp_sys_band.cbeta, np.cos(np.deg2rad(beta)))
-    np.testing.assert_equal(hwp_sys_band.cbetas, np.cos(np.deg2rad(beta)))
-    np.testing.assert_equal(hwp_sys_band.z1, z1)
-    np.testing.assert_equal(hwp_sys_band.z1s, z1)
-    np.testing.assert_equal(hwp_sys_band.z2, z2)
-    np.testing.assert_equal(hwp_sys_band.z2s, z2)
+    hwp_sys.fill_tod(obs_h,pointings,hwp_radpsec)
 
     reference = np.array([[ 3.8648595e-05, -3.9369585e-05, -3.7868820e-05, -3.8603906e-05,
         -3.8943563e-05, -3.8293791e-05, -3.8582919e-05, -3.8396141e-05,
@@ -128,6 +120,6 @@ def test_out_of_band():
         -3.7936523e-05, -3.8455721e-05]], dtype=np.float32)
 
 
-    np.testing.assert_equal(obs_o.tod, reference)
+    np.testing.assert_equal(obs_h.tod, reference)
 
 
