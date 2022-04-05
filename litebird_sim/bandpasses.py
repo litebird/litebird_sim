1f7c7e46beb3fcabc84a5beef68124a4731f2fc0
Luca Pagano <lu.pagano@gmail.com>
2022-04-05 22:37:17 +0200

    bands now in FreqChannelInfo, DetectorInfo and in obs

--- litebird_sim/bandpasses.py
+++ litebird_sim/bandpasses.py
@@ -22,8 +22,8 @@ class BandPassInfo(object):
       the :class:`.Imo` class).
 
     Args:
-        - nu_center (float):  center frequency in GHz
-        - bandwidth (float):  width of the band (default=0.2)
+        - bandcenter_ghz (float):  center frequency in GHz
+        - bandwidth_ghz (float):  width of the band (default=0.2)
         - range_offset (int): increase the freq. range by an offset (default +- 10 from the edges)
         - nsamp (int): number of elements to sample the band (default=128)
         - name (str) : ID of the band
@@ -53,8 +53,8 @@ class BandPassInfo(object):
 
         # we extend the wings of the top-hat bandpass  with 10 samples
         # before  and after the edges
-        bandrange = self.f0 - range_offset, self.f1 + range_offset
-        self.freqs = np.linspace(bandrange[0], bandrange[1], nsamp)
+        bandrange = self.f0 - self.range_offset, self.f1 + self.range_offset
+        self.freqs_ghz = np.linspace(bandrange[0], bandrange[1], self.nsamp)
         self.isnormalized = False
         if self.bandtype == "top-hat":
             self.get_top_hat_bandpass(normalize=self.normalize)
@@ -73,8 +73,8 @@ class BandPassInfo(object):
         get the edges of the tophat band
         """
         return (
-            self.bandcenter_ghz * (1 - self.bandwidth_ghz / 2),
-            self.bandcenter_ghz * (1 + self.bandwidth_ghz / 2),
+            self.bandcenter_ghz - self.bandwidth_ghz / 2,
+            self.bandcenter_ghz + self.bandwidth_ghz / 2,
         )
 
     def get_top_hat_bandpass(self, normalize=False, apodization=None):
@@ -87,8 +87,8 @@ class BandPassInfo(object):
         apodize the edges following the chosen  profile
         """
 
-        self.weights = np.zeros_like(self.freqs)
-        mask = np.ma.masked_inside(self.freqs, self.f0, self.f1).mask
+        self.weights = np.zeros_like(self.freqs_ghz)
+        mask = np.ma.masked_inside(self.freqs_ghz, self.f0, self.f1).mask
         self.weights[mask] = 1.0
 
         if apodization == "cosine":
@@ -110,7 +110,7 @@ class BandPassInfo(object):
         Normalize the band transmission coefficients
 
         """
-        A = np.trapz(self.weights, self.freqs)
+        A = np.trapz(self.weights, self.freqs_ghz)
         self.weights /= A
         self.isnormalized = True
 
@@ -125,10 +125,10 @@ class BandPassInfo(object):
         If alpha and beta are not specified a value of 1 is used for both.
 
         """
-        mask_beta = np.ma.masked_greater(self.freqs, self.f1).mask
-        self.weights[mask_beta] = np.exp(-beta * (self.freqs[mask_beta] - self.f1))
-        mask_alpha = np.ma.masked_less(self.freqs, self.f0).mask
-        self.weights[mask_alpha] = np.exp(alpha * (self.freqs[mask_alpha] - self.f0))
+        mask_beta = np.ma.masked_greater(self.freqs_ghz, self.f1).mask
+        self.weights[mask_beta] = np.exp(-beta * (self.freqs_ghz[mask_beta] - self.f1))
+        mask_alpha = np.ma.masked_less(self.freqs_ghz, self.f0).mask
+        self.weights[mask_alpha] = np.exp(alpha * (self.freqs_ghz[mask_alpha] - self.f0))
 
     def cosine_apodize_bandpass(self, a=5):
         """
@@ -143,14 +143,14 @@ class BandPassInfo(object):
         apod = lambda x, a, b: (1 + np.cos((x - a) / (b - a) * np.pi)) / 2
         f_above = self.bandcenter_ghz * (1 + self.bandwidth_ghz / 2 + apolength)
         f_below = self.bandcenter_ghz * (1 - self.bandwidth_ghz / 2 - apolength)
-        mask_above = np.ma.masked_inside(self.freqs, self.f1, f_above).mask
+        mask_above = np.ma.masked_inside(self.freqs_ghz, self.f1, f_above).mask
 
-        x_ab = np.linspace(self.f1, f_above, self.freqs[mask_above].size)
+        x_ab = np.linspace(self.f1, f_above, self.freqs_ghz[mask_above].size)
 
         self.weights[mask_above] = apod(x_ab, self.f1, f_above)
 
-        mask_below = np.ma.masked_inside(self.freqs, f_below, self.f0).mask
-        x_bel = np.linspace(f_below, self.f0, self.freqs[mask_below].size)
+        mask_below = np.ma.masked_inside(self.freqs_ghz, f_below, self.f0).mask
+        x_bel = np.linspace(f_below, self.f0, self.freqs_ghz[mask_below].size)
         self.weights[mask_below] = apod(x_bel, self.f0, f_below)
 
     # Chebyshev profile bandpass
@@ -172,7 +172,7 @@ class BandPassInfo(object):
             "bandpass",
             analog=True,
         )
-        w, h = signal.freqs(b, a, worN=self.freqs * 2 * np.pi * 1e9)
+        w, h = signal.freqs(b, a, worN=self.freqs_ghz * 2 * np.pi * 1e9)
 
         self.weights = abs(h)
         if normalize:
@@ -184,7 +184,7 @@ class BandPassInfo(object):
         """
         Estimate the integral over the frequency band
         """
-        return np.trapz(self.weights, self.freqs)
+        return np.trapz(self.weights, self.freqs_ghz)
 
     # Find effective central frequency of a bandpass profile
     def find_central_frequency(self):
@@ -192,10 +192,10 @@ class BandPassInfo(object):
         a bandpass profile as defined in https://arxiv.org/abs/1303.5070
         """
         if self.isnormalized:
-            return np.trapz(self.freqs * self.weights, self.freqs)
+            return np.trapz(self.freqs_ghz * self.weights, self.freqs_ghz)
         else:
             return (
-                np.trapz(self.freqs * self.weights, self.freqs)
+                np.trapz(self.freqs_ghz * self.weights, self.freqs_ghz)
                 / self.get_normalization()
             )
 
@@ -219,18 +219,18 @@ class BandPassInfo(object):
         if not self.isnormalized:
             self.normalize_band()
         # Interpolate the band
-        b = sp.interpolate.interp1d(x=self.freqs, y=self.weights)
+        b = sp.interpolate.interp1d(x=self.freqs_ghz, y=self.weights)
         # estimate the CDF
         Pnu = np.array(
             [
-                sp.integrate.quad(b, a=self.freqs.min(), b=inu)[0]
-                for inu in self.freqs[1:]
+                sp.integrate.quad(b, a=self.freqs_ghz.min(), b=inu)[0]
+                for inu in self.freqs_ghz[1:]
             ]
         )
         # interpolate the inverse CDF
         self.Sampler = sp.interpolate.interp1d(
             Pnu,
-            self.freqs[:-1] + np.diff(self.freqs),
+            self.freqs_ghz[:-1] + np.diff(self.freqs_ghz),
             bounds_error=False,
             fill_value="extrapolate",
         )
@@ -260,16 +260,16 @@ class BandPassInfo(object):
                 Sampler = self.Sampler
 
         X = np.random.uniform(size=bstrap_size)
-        bins_nu = np.linspace(self.freqs.min(), self.freqs.max(), nresample)
+        bins_nu = np.linspace(self.freqs_ghz.min(), self.freqs_ghz.max(), nresample)
         h, xb = np.histogram(Sampler(X), density=True, bins=bins_nu)
 
         nu_b = xb[:-1] + np.diff(xb)
         resampled_bpass = abs(
             sp.interpolate.interp1d(
                 nu_b, h, kind="cubic", bounds_error=False, fill_value="extrapolate"
-            )(self.freqs)
+            )(self.freqs_ghz)
         )
         if self.isnormalized:
-            return resampled_bpass / np.trapz(resampled_bpass, self.freqs)
+            return resampled_bpass / np.trapz(resampled_bpass, self.freqs_ghz)
         else:
             return resampled_bpass
