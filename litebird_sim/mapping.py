6d441b7ce1888f4b47b1142ce0f2a05b51718685
Luca Pagano <lu.pagano@gmail.com>
2022-05-24 17:31:17 +0200

    noise weighting added

--- litebird_sim/mapping.py
+++ litebird_sim/mapping.py
@@ -15,26 +15,29 @@ COND_THRESHOLD = 1e10
 
 
 @njit
-def _accumulate_map_and_info(tod, pix, psi, info):
+def _accumulate_map_and_info(tod, pix, psi, weights, info):
     # Fill the upper triangle of the information matrix and use the lower
     # triangle for the RHS of the map-making equation
     assert tod.shape == pix.shape == psi.shape
-    tod = tod.ravel()
-    pix = pix.ravel()
-    psi = psi.ravel()
-    for d, p, a in zip(tod, pix, psi):
-        cos = np.cos(2 * a)
-        sin = np.sin(2 * a)
-        info_pix = info[p]
-        info_pix[0, 0] += 1.0
-        info_pix[0, 1] += cos
-        info_pix[0, 2] += sin
-        info_pix[1, 1] += cos * cos
-        info_pix[1, 2] += sin * cos
-        info_pix[2, 2] += sin * sin
-        info_pix[1, 0] += d
-        info_pix[2, 0] += d * cos
-        info_pix[2, 1] += d * sin
+
+    ndets = tod.shape[0]
+    nsamples = tod.shape[1]
+
+    for idet in range(ndets):
+        for d, p, a in zip(tod[idet], pix[idet], psi[idet]):
+            one = 1.0 / np.sqrt(weights[idet])
+            cos = np.cos(2 * a) / np.sqrt(weights[idet])
+            sin = np.sin(2 * a) / np.sqrt(weights[idet])
+            info_pix = info[p]
+            info_pix[0, 0] += one * one
+            info_pix[0, 1] += one * cos
+            info_pix[0, 2] += one * sin
+            info_pix[1, 1] += cos * cos
+            info_pix[1, 2] += sin * cos
+            info_pix[2, 2] += sin * sin
+            info_pix[1, 0] += d * one * one
+            info_pix[2, 0] += d * cos * one
+            info_pix[2, 1] += d * sin * one
 
 
 def _extract_map_and_fill_info(info):
@@ -47,7 +50,9 @@ def _extract_map_and_fill_info(info):
     return rhs
 
 
-def make_bin_map(obss: Union[Observation, List[Observation]], nside):
+def make_bin_map(
+    obss: Union[Observation, List[Observation]], nside, docovariance=False
+):
     """Bin Map-maker
 
     Map a list of observations
@@ -80,7 +85,8 @@ def make_bin_map(obss: Union[Observation, List[Observation]], nside):
         obs_list = obss
 
     for obs in obs_list:
-        _accumulate_map_and_info(obs.tod, obs.pixind, obs.psi, info)
+        weights = obs.sampling_rate_hz * obs.net_ukrts ** 2
+        _accumulate_map_and_info(obs.tod, obs.pixind, obs.psi, weights, info)
 
     if all([obs.comm is None for obs in obs_list]) or not mpi.MPI_ENABLED:
         # Serial call
@@ -99,10 +105,17 @@ def make_bin_map(obss: Union[Observation, List[Observation]], nside):
 
     rhs = _extract_map_and_fill_info(info)
     try:
-        return np.linalg.solve(info, rhs)
+        res = np.linalg.solve(info, rhs)
     except np.linalg.LinAlgError:
         cond = np.linalg.cond(info)
         res = np.full_like(rhs, hp.UNSEEN)
         mask = cond < COND_THRESHOLD
         res[mask] = np.linalg.solve(info[mask], rhs[mask])
-        return res
+
+    if docovariance:
+        try:
+            return res, np.linalg.inv(info)
+        except np.linalg.LinAlgError:
+            covmat = np.full_like(info, hp.UNSEEN)
+            covmat[mask] = np.linalg.inv(info[mask])
+            return res, covmat
