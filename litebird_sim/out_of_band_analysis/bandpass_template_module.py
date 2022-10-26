2943d9515850e0b9a02366621f219af97bb4dc8f
sgiardie <grdsrn1@unife.it>
2022-10-26 09:55:27 +0200

    interfacing out_of_band with bandpassinfo class + reformatting. Adding limits of bandpass in bandpassinfo class

--- litebird_sim/out_of_band_analysis/bandpass_template_module.py
+++ litebird_sim/out_of_band_analysis/bandpass_template_module.py
@@ -2,88 +2,12 @@ import litebird_sim as lbs
 import numpy as np
 from typing import Union, List
 from scipy import signal
-
-# Top-hat Bandpass
-def top_hat_bandpass(freqs, f0, f1):
-    """Define a top-hat bandpass
-    freqs: frequency in GHz
-    f0: low-frequency edge of the top-hat in GHz
-    f1: high-frequency edge of the top-hat in GHz
-    """
-    transmission = np.zeros_like(freqs)
-
-    for i in range(len(freqs)):
-
-        if freqs[i] >= f0 and freqs[i] <= f1:
-
-            transmission[i] = 1.0
-
-        else:
-
-            transmission[i] = 0.0
-
-    return transmission
-
-
-# Top-hat bandpass with exponential wings
-def top_hat_bandpass_with_exp_wings(freqs, f0, f1, alpha=1, beta=1):
-    """Define a bandpass with exponential tails and unit transmission in band
-    freqs: frequency in GHz
-    f0: low-frequency edge of the band in GHz
-    f1: high-frequency edge of the band in GHz
-    alpha: out-of-band exponential decay index for low freq edge
-    beta: out-of-band exponential decay index for high freq edge
-
-    If alpha and beta are not specified a value of 1 is used for both.
-
-    """
-    transmission = np.zeros_like(freqs)
-
-    for i in range(len(freqs)):
-
-        if freqs[i] >= f0 and freqs[i] <= f1:
-
-            transmission[i] = 1.0
-
-        elif freqs[i] > f1:
-
-            transmission[i] = np.exp(-beta * (freqs[i] - f1))
-
-        elif freqs[i] < f0:
-
-            transmission[i] = np.exp(alpha * (freqs[i] - f0))
-
-    return transmission
-
-
-# Chebyshev profile bandpass
-def bandpass_chebyshev(freqs, f0, f1, order=3, ripple_dB=3):
-    """Define a bandpass with chebyshev prototype
-    freqs: frequency in GHz
-    f0: low-frequency edge of the band in GHz
-    f1: high-frequency edge of the band in GHz
-    order: chebyshev filter order
-    ripple_dB: maximum ripple amplitude in decibels
-
-    If order and ripple_dB are not specified a value of 3 is used for both.
-
-    """
-    b, a = signal.cheby1(
-        order,
-        ripple_dB,
-        [2.0 * np.pi * f0 * 1e9, 2.0 * np.pi * f1 * 1e9],
-        "bandpass",
-        analog=True,
-    )
-    w, h = signal.freqs(b, a, worN=freqs * 2 * np.pi * 1e9)
-
-    transmission = abs(h)
-
-    return transmission
+from litebird_sim import BandPassInfo
 
 
 # Chebyshev profile lowpass
 def lowpass_chebyshev(freqs, f0, order=1, ripple_dB=1):
+
     """Define a lowpass with chebyshev prototype
     freqs: frequency in GHz
     f0: low-frequency edge of the band in GHz
@@ -122,11 +46,13 @@ def add_high_frequency_transmission(freqs, bandpass, location=3, transmission=0.
     """Add high frequency leakage
     freqs: frequency in GHz
     bandpass: transmission profile
-    location: multiple of the central frequency of the bandpass profile where add the leakage
-    transmission: relative amplitude of the high frequency leakage with respect to the nominal band
+    location: multiple of the central frequency of the bandpass profile
+    where add the leakage
+    transmission: relative amplitude of the high frequency leakage
+    with respect to the nominal band
 
-    If location and transmission are not specified a value of 3 and 0.5 are set by default.
-
+    If location and transmission are not specified a value of 3
+    and 0.5 are set by default.
     """
 
     df = freqs[1] - freqs[0]
@@ -145,7 +71,8 @@ def add_high_frequency_transmission(freqs, bandpass, location=3, transmission=0.
         freqs[0], new_freqs_max, int((new_freqs_max - new_freqs_min) / df + 1)
     )
     bandpass_new = np.zeros_like(freqs_new)
-    # finding the shifted position of freqs[0] i.e. the position in freqs_new from which the band is transmitted at a higher frequency
+    # finding the shifted position of freqs[0] i.e. the position
+    # in freqs_new from which the band is transmitted at a higher frequency
     i_nf0 = np.where(np.round(freqs_new - (high_freq_fc - delta_fc)) == 0)[0][0]
 
     for i in range(len(freqs_new)):
@@ -183,63 +110,40 @@ def bandpass_profile(
 
     elif "band_type" in bandpass.keys():
 
-        if bandpass["band_type"] == "top_hat":
+        if "band_alpha" not in bandpass.keys():
+            bandpass["band_alpha"] = 1
+        if "band_beta" not in bandpass.keys():
+            bandpass["band_beta"] = 1
+        if "cosine_apo_length" not in bandpass.keys():
+            bandpass["cosine_apo_length"] = 5
+        if "band_order" not in bandpass.keys():
+            bandpass["band_order"] = 3
+        if "band_ripple_dB" not in bandpass.keys():
+            bandpass["band_ripple_dB"] = 3
+        if "normalize" not in bandpass.keys():
+            bandpass["normalize"] = False
 
-            profile = top_hat_bandpass(
-                freqs,
-                bandpass["band_low_edge"],
-                bandpass["band_high_edge"],
-            )
+        bandclass = BandPassInfo(
+            bandcenter_ghz=bandpass["bandcenter_ghz"],
+            bandwidth_ghz=bandpass["band_high_edge"] - bandpass["band_low_edge"],
+            bandlow_ghz=freqs[0],
+            bandhigh_ghz=freqs[-1],
+            nsamples_inband=freqs.size,
+            alpha_exp=bandpass["band_alpha"],
+            beta_exp=bandpass["band_beta"],
+            cosine_apo_length=bandpass["cosine_apo_length"],
+            cheby_poly_order=bandpass["band_order"],
+            cheby_ripple_dB=bandpass["band_ripple_dB"],
+            normalize=bandpass["normalize"],
+            bandtype=bandpass["band_type"],
+        )
 
-        elif bandpass["band_type"] == "top_hat_with_exp_wings":
-
-            if (
-                "band_alpha" not in bandpass.keys()
-                and "band_beta" not in bandpass.keys()
-            ):
-
-                profile = top_hat_bandpass_with_exp_wings(
-                    freqs,
-                    bandpass["band_low_edge"],
-                    bandpass["band_high_edge"],
-                )
-
-            else:
-
-                profile = top_hat_bandpass_with_exp_wings(
-                    freqs,
-                    bandpass["band_low_edge"],
-                    bandpass["band_high_edge"],
-                    bandpass["band_alpha"],
-                    bandpass["band_beta"],
-                )
-
-        elif bandpass["band_type"] == "chebyshev":
-
-            if (
-                "band_order" not in bandpass.keys()
-                and "band_ripple_dB" not in bandpass.keys()
-            ):
-
-                profile = bandpass_chebyshev(
-                    freqs,
-                    bandpass["band_low_edge"],
-                    bandpass["band_high_edge"],
-                )
-
-            else:
-
-                profile = bandpass_chebyshev(
-                    freqs,
-                    bandpass["band_low_edge"],
-                    bandpass["band_high_edge"],
-                    bandpass["band_order"],
-                    bandpass["band_ripple_dB"],
-                )
+        freqs = bandclass.freqs_ghz
+        profile = bandclass.weights
 
     if (
         "band_high_freq_leak" in bandpass.keys()
-        and bandpass["band_high_freq_leak"] == True
+        and bandpass["band_high_freq_leak"] is True
     ):
 
         if (
@@ -258,7 +162,7 @@ def bandpass_profile(
                 bandpass["band_high_freq_trans"],
             )
 
-    if include_beam_throughput == True:
+    if include_beam_throughput is True:
 
         profile = profile * beam_throughtput(freqs)
 
