c320d1e414e43633ed1cf519546dae5d5998c904
Maurizio Tomasi <ziotom78@gmail.com>
2024-01-03 14:06:02 +0100

    Update the documentation to mention ruff instead of flake8/black

--- bin/run_tests.bat
+++ bin/run_tests.bat
@@ -1,10 +1,10 @@
 set /a MAX_LINE_LENGTH = 88
 
 rem Verify that the code is properly formatted
-poetry run black --check --line-length %MAX_LINE_LENGTH% -q .
+poetry run ruff format --diff .
 
 rem Check for common errors
-poetry run flake8 --max-line-length %MAX_LINE_LENGTH%
+poetry run ruff check .
 
 rem Run the test suite
 poetry run python3 -m pytest --doctest-modules -vv
