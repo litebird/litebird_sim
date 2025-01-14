e33575bf8a2ef48ee9b570a95ce2b1b627bc1814
Maurizio Tomasi <ziotom78@gmail.com>
2025-01-14 16:40:07 +0100

    Add tests for the SphericalHarmonics constructor

--- test/test_spherical_harmonics.py
+++ test/test_spherical_harmonics.py
@@ -1,9 +1,44 @@
 # -*- encoding: utf-8 -*-
 
+import healpy
 import numpy as np
 import pytest
 
 from litebird_sim import SphericalHarmonics
+
+
+def test_constructor():
+    nside = 16
+    lmax = 10
+
+    # Only temperature
+    harmonics = SphericalHarmonics(
+        values=healpy.map2alm(
+            maps=np.random.rand(healpy.nside2npix(nside)), lmax=lmax, mmax=lmax
+        ),
+        lmax=lmax,
+    )
+    assert harmonics.nstokes == 1
+    assert harmonics.values.shape[1] == SphericalHarmonics.num_of_alm_from_lmax(
+        lmax=lmax
+    )
+
+    harmonics = SphericalHarmonics(
+        values=healpy.map2alm(
+            maps=np.random.rand(3, healpy.nside2npix(nside)), lmax=lmax, mmax=lmax
+        ),
+        lmax=lmax,
+    )
+    assert harmonics.nstokes == 3
+    assert len(harmonics.values[0, :]) == SphericalHarmonics.num_of_alm_from_lmax(
+        lmax=lmax
+    )
+    assert len(harmonics.values[1, :]) == SphericalHarmonics.num_of_alm_from_lmax(
+        lmax=lmax
+    )
+    assert len(harmonics.values[2, :]) == SphericalHarmonics.num_of_alm_from_lmax(
+        lmax=lmax
+    )
 
 
 def test_num_of_alm_coefficients():
