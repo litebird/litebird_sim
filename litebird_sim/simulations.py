d39a082b2bd0a86d282ff5c58717f36d358ba66d
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2025-05-22 19:14:30 +0200

    updated the interface to brahmap and added the docstring

--- litebird_sim/simulations.py
+++ litebird_sim/simulations.py
@@ -13,8 +13,6 @@ from pathlib import Path
 from shutil import copyfile, copytree, SameFileError
 from typing import List, Tuple, Union, Dict, Any, Optional
 from uuid import uuid4
-from importlib.util import find_spec
-import types
 
 import astropy.time
 import astropy.units
@@ -480,47 +478,6 @@ class Simulation:
         # Initialize self.random. The user is free to
         # call self.init_random() again later
         self.init_random(self.random_seed)
-
-        if find_spec("brahmap") is not None:
-            import brahmap  # noqa: E402
-
-            def _make_brahmap_gls_map(
-                self,
-                nside,
-                component: Optional[List[str]] = "tod",
-                inv_noise_cov_operator=None,
-                threshold: float = 1.0e-5,
-                pointings_dtype=np.float64,
-                gls_params: brahmap.LBSimProcessTimeSamples = None,
-            ):
-                if gls_params is None:
-                    gls_params = brahmap.LBSimGLSParameters()
-
-                gls_results = brahmap.LBSim_compute_GLS_maps(
-                    nside=nside,
-                    observations=self.observations,
-                    component=component,
-                    pointings_flag=None,
-                    inv_noise_cov_operator=inv_noise_cov_operator,
-                    threshold=threshold,
-                    dtype_float=pointings_dtype,
-                    LBSim_gls_parameters=gls_params,
-                )
-
-                return gls_results
-
-        else:
-
-            def _make_brahmap_gls_map(self, **args):
-                raise ImportError(
-                    "Could not import `BrahMap`. Make sure that "
-                    "the package `BrahMap` is installed in the same environment "
-                    "as `litebird_sim`. Refer to "
-                    "https://anand-avinash.github.io/BrahMap/overview/installation/ "
-                    "for the installation instruction"
-                )
-
-        self.make_brahmap_gls_map = types.MethodType(_make_brahmap_gls_map, self)
 
     def init_random(self, random_seed):
         """
@@ -2175,6 +2132,115 @@ class Simulation:
                 results=results,
                 bytes_in_cholesky_matrices=results.nobs_matrix_cholesky.nbytes,
             )
+
+    @_profile
+    def make_brahmap_gls_map(
+        self,
+        nside: int,
+        component: str = "tod",
+        pointing_flag: np.ndarray = None,
+        inv_noise_cov_operator=None,
+        threshold: float = 1.0e-5,
+        pointings_dtype=np.float64,
+        gls_params: "brahmap.LBSimProcessTimeSamples" = None,  # noqa
+    ) -> Union[
+        "brahmap.LBSimGLSResult",  # noqa
+        tuple["brahmap.LBSimProcessTimeSamples", "brahmap.LBSimGLSResult"],  # noqa
+    ]:
+        """Wrapper to the GLS map-maker of BrahMap.
+
+        This function allows the users to do the optimal map-making with
+        BrahMap. Since BrahMap is seemlessly interfaced with LBS, it allows
+        the map-making without storing the TODs on the disk. The function
+        needs an inverse noise covariance operator and an object containing
+        the GLS parameters as arguments.
+
+        BrahMap offers a variety of noise covariance opeartors, all
+        compatible with LBS. Noise covariance operator for white noise, for
+        example, can be defined as::
+
+            inv_cov = brahmap.LBSim_InvNoiseCovLO_UnCorr(
+                obs = ...,
+                inverse_noise_variance = ...,
+                dtype = ...,
+            )
+
+        The parameters used for GLS can be defined as::
+
+            gls_params = brahmap.LBSimGLSParameters(
+                output_coordinate_system = lbs.CoordinateSystem.Galactic,
+                return_processed_samples = False,
+                solver_type = brahmap.SolverType.IQU,
+                use_preconditioner = True,
+                preconditioner_threshold = 1.0e-25,
+                preconditioner_max_iterations = 100,
+                return_hit_map = False,
+            )
+
+        For the complete list of available noise covariance operators and
+        advance usage, please refer to the BrahMap documentation:
+        https://anand-avinash.github.io/BrahMap/
+
+        Parameters
+        ----------
+        nside : int
+            Nside of the output map
+        component : str, optional
+            The TOD component to be used for map-making, by default "tod"
+        inv_noise_cov_operator : optional
+            Inverse noise covariance operator, by default None
+        threshold : float, optional
+            Threshold parameter to determine the poorly observed pixels, by
+            default 1.0e-5
+        pointings_dtype : dtype, optional
+            dtype to be used for computing pointings on the fly. The
+            `pointing_dtype` must be same as the dtype of the TOD; by default
+            `np.float64`
+        gls_params : LBSimProcessTimeSamples, optional
+            An object that encapsulates the parameter of the GLS, including
+            PCG threshold and max iteration; by default None
+
+        Returns
+        -------
+        Union[LBSimGLSResult, tuple[LBSimProcessTimeSamples, LBSimGLSResult]]
+            Returns an `LBSimGLSResult` object when
+            `gls_params.return_processed_samples = False`. `LBSimGLSResult`
+            object encapsulates the output of GLS including the output maps
+            and PCG convergence status. The function returns the tuple object
+            `(LBSimProcessTimeSamples, LBSimGLSResult)` when
+            `gls_params.return_processed_samples = True`.
+
+        Raises
+        ------
+        ImportError
+            Raises `ImportError` when the brahmap package couldn't be imported
+        """
+        try:
+            import brahmap
+        except ImportError:
+            raise ImportError(
+                "Could not import `BrahMap`. Make sure that the package "
+                "`BrahMap` is installed in the same environment "
+                "as `litebird_sim`. Refer to "
+                "https://anand-avinash.github.io/BrahMap/overview/installation/ "
+                "for the installation instruction"
+            )
+
+        if gls_params is None:
+            gls_params = brahmap.LBSimGLSParameters()
+
+        gls_result = brahmap.LBSim_compute_GLS_maps(
+            nside=nside,
+            observations=self.observations,
+            component=component,
+            pointings_flag=pointing_flag,
+            inv_noise_cov_operator=inv_noise_cov_operator,
+            threshold=threshold,
+            dtype_float=pointings_dtype,
+            LBSim_gls_parameters=gls_params,
+        )
+
+        return gls_result
 
     @_profile
     def write_observations(
