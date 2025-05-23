93bd55c87f68e500372cc776ad8b049be1f1417a
Silvia Micheli <117379461+silviamich@users.noreply.github.com>
2025-05-23 16:18:25 +0200

    fix hwp_diff_emiss arguments

--- litebird_sim/simulations.py
+++ litebird_sim/simulations.py
@@ -1646,7 +1646,6 @@ class Simulation:
         self,
         component: str = "tod",
         amplitude_2f_k: Union[float, None] = None,
-        optical_power_k: Union[float, None] = None,
         append_to_report: bool = False,
     ):
         """Add the HWP differential emission to all the observations of this
@@ -1663,7 +1662,6 @@ class Simulation:
             hwp=self.hwp,
             component=component,
             amplitude_2f_k=amplitude_2f_k,
-            optical_power_k=optical_power_k,
         )
 
         if append_to_report and MPI_COMM_WORLD.rank == 0:
@@ -1677,15 +1675,9 @@ class Simulation:
             else:
                 amp = amplitude_2f_k
 
-            if optical_power_k is None:
-                op = "Optical power taken from IMo"
-            else:
-                op = optical_power_k
-
             self.append_to_report(
                 markdown_template,
                 amplitude_2f=amp,
-                optical_power=op,
             )
 
     @_profile
