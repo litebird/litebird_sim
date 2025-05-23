8dc7189244e29a5250e11f0cdb65f58d7a6c7fc7
Maurizio Tomasi <ziotom78@gmail.com>
2025-05-23 10:36:24 +0200

    Re-fix issue in prepare_pointings
    Apparently, the fix disappeared in one of the merge conflicts we have
    had in the last days

--- litebird_sim/observations.py
+++ litebird_sim/observations.py
@@ -821,7 +821,7 @@ class Observation:
             )
             self.has_hwp = False
         else:
-            for idet in self.det_idx:
+            for idet in range(len(self.det_idx)):
                 if self.mueller_hwp[idet] is None:
                     self.mueller_hwp[idet] = hwp.mueller
             self.has_hwp = True
