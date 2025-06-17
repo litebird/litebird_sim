fb155f999c0bd537defb0b7174222f837c950949
Marta Monelli <di38wul@cm4login2.cos.lrz.de>
2025-06-17 13:30:40 +0200

    added assert to make sure that nside_out ≤ nside

--- litebird_sim/hwp_sys/hwp_sys.py
+++ litebird_sim/hwp_sys/hwp_sys.py
@@ -503,6 +503,10 @@ class HwpSys:
 
             self.nside = paramdict.get("nside", False)
             self.nside_out = paramdict.get("nside_out", False)
+            
+            assert self.nside_out <= self.nside, (
+                    "Error, nside_out cannot be larger than nside"
+            )
 
             self.build_map_on_the_fly = paramdict.get("build_map_on_the_fly", False)
 
