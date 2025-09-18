fa563c552b73d7b205a89cde610f722f49e58be5
Giacomo Galloni <giacomo.galloni@unife.it>
2025-09-18 18:00:44 +0200

    update bins

--- bin/run_tests.bat
+++ bin/run_tests.bat
@@ -1,10 +1,10 @@
 set /a MAX_LINE_LENGTH = 88
 
 rem Verify that the code is properly formatted
-poetry run ruff format --diff .
+uv run ruff format --diff .
 
 rem Check for common errors
-poetry run ruff check .
+uv run ruff check .
 
 rem Run the test suite
-poetry run python3 -m pytest --doctest-modules -vv
+uv run python -m pytest --doctest-modules -vv
