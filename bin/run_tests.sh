d8924341342ee0347895bec659ac1e711752c985
Giacomo Galloni <giacomo.galloni@unife.it>
2025-10-01 14:40:05 +0200

    keep convenience scripts for CI tests

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -2,12 +2,6 @@
 
 set -o errexit
 
-# Verify that the code is properly formatted
-uv run ruff format --diff .
-
-# Check for common errors
-uv run ruff check .
-
 # Run the test suite and print the N slowest tests
 uv run python -m pytest --cov=litebird_sim --cov-report=xml --cov-report=term --durations=5 -vv
 
