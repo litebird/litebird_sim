b785bb694598a10ddc568ba8da69421a95498104
Camille Demay <camille.demay@proton.me>
2026-02-06 14:20:28 +0900

    [skip ci] filename change

--- litebird_sim/mapmaking/__init__.py
+++ litebird_sim/mapmaking/__init__.py
@@ -1,6 +1,6 @@
 from .common import ExternalDestriperParameters
 from .binner import make_binned_map, check_valid_splits, BinnerResult
-from .h_n import HnMapResult, make_h_maps,load_h_map_from_file
+from .h_maps import HnMapResult, make_h_maps,load_h_map_from_file
 from .brahmap_gls import make_brahmap_gls_map
 from .destriper import (
     make_destriped_map,
